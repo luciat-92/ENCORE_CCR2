62aaf7c6887c9fbefcf9048933e491f8866a6fc8
Lucia Trastulla <lucia.trastulla@fht.org>
2023-10-19 19:39:04 +0200

    renv setting

---
+++ setup_renv.R
@@ -0,0 +1,25 @@
+## setup of renv (ccr2 under development)
+library("renv")
+
+#### INITIALIZE #####
+renv::init(
+  settings = list(snapshot.type = "implicit", # default, Capture only packages which appear to be used in your project
+                  package.dependency.fields = c("Imports", "Depends", "LinkingTo", "Remotes")), # When explicitly installing a package with install(), what fields should be used to determine that packages dependencies?
+)
+renv::activate()
+
+#### INSTALL ####
+renv::install("devtools")
+renv::install("tidyverse")
+renv::install("pak")
+
+# add github credential PAT (only needed if repo is not public)
+library(devtools)
+library(pak)
+gitcreds::gitcreds_set()
+
+pak::pkg_install("luciat-92/CRISPRcleanRatSquared") # if using renv::install, there are problems with the dependencies of ccr
+
+#### SNAPSHOT ####
+renv::snapshot()
+
